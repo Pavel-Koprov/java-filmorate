c9e1d9b1ad228455c175a6404cf6029584fcabdd
Pavel Koprov <pkoprov@yandex.ru>
2024-09-22 00:17:26 +0300

    Исправил ошибки

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.ValidationExceptions;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
@@ -26,24 +26,24 @@ class FilmControllerTest {
     @Test
     void ifNameEmptyError() {
         film.setName("");
-        assertThrows(ValidationException.class, () -> filmController.create(film));
+        assertThrows(ValidationExceptions.class, () -> filmController.create(film));
     }
 
     @Test
     void ifDescriptionMoreThan200Error() {
         film.setDescription("f".repeat(201));
-        assertThrows(ValidationException.class, () -> filmController.create(film));
+        assertThrows(ValidationExceptions.class, () -> filmController.create(film));
     }
 
     @Test
     void ifReleaseDateBefore28121985Error() {
         film.setReleaseDate(LocalDate.of(1800,1,1));
-        assertThrows(ValidationException.class, () -> filmController.create(film));
+        assertThrows(ValidationExceptions.class, () -> filmController.create(film));
     }
 
     @Test
     void ifDurationLessThan0Error() {
         film.setDuration(0L);
-        assertThrows(ValidationException.class, () -> filmController.create(film));
+        assertThrows(ValidationExceptions.class, () -> filmController.create(film));
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.ValidationExceptions;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
@@ -27,31 +27,30 @@ class UserControllerTest {
     @Test
     void ifNotContainsDogError() {
         user.setEmail("blankemail.ru");
-        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertThrows(ValidationExceptions.class, () -> userController.create(user));
     }
 
     @Test
     void ifEmptyEmailError() {
         user.setEmail("");
-        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertThrows(ValidationExceptions.class, () -> userController.create(user));
     }
 
     @Test
     void ifEmptyLoginError() {
         user.setLogin("");
-        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertThrows(ValidationExceptions.class, () -> userController.create(user));
     }
 
     @Test
     void ifContainsSpaceLoginError() {
         user.setLogin("login login");
-        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertThrows(ValidationExceptions.class, () -> userController.create(user));
     }
 
     @Test
     void ifBirthdayAfterNowError() {
         user.setBirthday(LocalDate.now().plusDays(1));
-        assertThrows(ValidationException.class, () -> userController.create(user));
+        assertThrows(ValidationExceptions.class, () -> userController.create(user));
     }
-
 }
